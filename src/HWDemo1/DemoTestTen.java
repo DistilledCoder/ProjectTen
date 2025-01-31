dfed90527a779ed15b06cb0aebbd40dee229266d
ericw <ericw@UncleRicco.san.rr.com>
2025-01-30 22:25:25 -0800

    Editing to get the reps in...

--- src/HWDemo1/DemoTestTen.java
+++ src/HWDemo1/DemoTestTen.java
@@ -5,6 +5,8 @@ public class DemoTestTen {
 public static void main (String args []) {
 
   System.out.println("Let's get used to doing edits like this");
+  
+  System.out.println("Another edit for my learning process");
 
 }
   
